008b6a71a0b2eead7baee45bda23b6c3b4421afa
RickHutten <rick.hutten@gmail.com>
2022-07-12 11:10:16 +0200

    gateway start

---
+++ gateway/src/main/java/com/ordina/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.ordina.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r.path("/api/v1/auth/*")
+                        .uri("http://auth-service:8081"))
+                .build();
+    }
+}
