2aa6e9ad2ede5aa3baead7fc2bc3596c5a270d48
RickHutten <rick.hutten@gmail.com>
2022-07-18 14:11:28 +0200

    changed to timestamp in sql object

--- message-service/src/main/java/com/ordina/messageservice/message/Message.java
+++ message-service/src/main/java/com/ordina/messageservice/message/Message.java
@@ -4,12 +4,12 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
+import java.util.Objects;
 
 @Getter
 @Entity
 @Builder
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "messages")
@@ -28,5 +28,25 @@ public class Message {
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private Timestamp createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Message message = (Message) o;
+
+        if (!Objects.equals(userId, message.userId)) return false;
+        if (!Objects.equals(content, message.content)) return false;
+        return Objects.equals(createdAt, message.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        return result;
+    }
 }
